fd53b1b6ece42f503be7a1744b8cff390febef13
blockyadv <135028044+blockyadv@users.noreply.github.com>
2023-05-30 08:34:46 -0400

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,367 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.titlelabel = New System.Windows.Forms.Label()
+        Me.titlepic8 = New System.Windows.Forms.PictureBox()
+        Me.titlepic7 = New System.Windows.Forms.PictureBox()
+        Me.titlepic6 = New System.Windows.Forms.PictureBox()
+        Me.titlepic5 = New System.Windows.Forms.PictureBox()
+        Me.titlepic3 = New System.Windows.Forms.PictureBox()
+        Me.titlepic2 = New System.Windows.Forms.PictureBox()
+        Me.titlepic = New System.Windows.Forms.PictureBox()
+        Me.titlepic9 = New System.Windows.Forms.PictureBox()
+        Me.titlepic10 = New System.Windows.Forms.PictureBox()
+        Me.titlepic11 = New System.Windows.Forms.PictureBox()
+        Me.scoretimer = New System.Windows.Forms.Timer(Me.components)
+        Me.scorelabel = New System.Windows.Forms.Label()
+        Me.scoretext = New System.Windows.Forms.Label()
+        Me.platform1 = New System.Windows.Forms.PictureBox()
+        Me.player1 = New System.Windows.Forms.PictureBox()
+        Me.gametimer = New System.Windows.Forms.Timer(Me.components)
+        Me.spike1 = New System.Windows.Forms.PictureBox()
+        Me.platform2 = New System.Windows.Forms.PictureBox()
+        Me.startgamelabel = New System.Windows.Forms.Label()
+        Me.howtoplaylabel = New System.Windows.Forms.Label()
+        Me.creditslabel = New System.Windows.Forms.Label()
+        Me.jumptimer = New System.Windows.Forms.Timer(Me.components)
+        Me.skintimer = New System.Windows.Forms.Timer(Me.components)
+        Me.movingtimer = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.titlepic8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.titlepic7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.titlepic6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.titlepic5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.titlepic3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.titlepic2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.titlepic, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.titlepic9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.titlepic10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.titlepic11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.platform1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.player1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.spike1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.platform2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'titlelabel
+        '
+        Me.titlelabel.Font = New System.Drawing.Font("Haettenschweiler", 102.0!)
+        Me.titlelabel.Location = New System.Drawing.Point(188, 9)
+        Me.titlelabel.Name = "titlelabel"
+        Me.titlelabel.Size = New System.Drawing.Size(875, 276)
+        Me.titlelabel.TabIndex = 1
+        Me.titlelabel.Text = "Blocky Adventure"
+        '
+        'titlepic8
+        '
+        Me.titlepic8.Image = Global.Blocky_Adventure.My.Resources.Resources.player2
+        Me.titlepic8.Location = New System.Drawing.Point(946, 259)
+        Me.titlepic8.Name = "titlepic8"
+        Me.titlepic8.Size = New System.Drawing.Size(65, 65)
+        Me.titlepic8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.titlepic8.TabIndex = 11
+        Me.titlepic8.TabStop = False
+        '
+        'titlepic7
+        '
+        Me.titlepic7.Image = Global.Blocky_Adventure.My.Resources.Resources.spike1
+        Me.titlepic7.Location = New System.Drawing.Point(850, 358)
+        Me.titlepic7.Name = "titlepic7"
+        Me.titlepic7.Size = New System.Drawing.Size(65, 65)
+        Me.titlepic7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.titlepic7.TabIndex = 10
+        Me.titlepic7.TabStop = False
+        '
+        'titlepic6
+        '
+        Me.titlepic6.Image = Global.Blocky_Adventure.My.Resources.Resources.spike1
+        Me.titlepic6.Location = New System.Drawing.Point(1069, 249)
+        Me.titlepic6.Name = "titlepic6"
+        Me.titlepic6.Size = New System.Drawing.Size(65, 65)
+        Me.titlepic6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.titlepic6.TabIndex = 9
+        Me.titlepic6.TabStop = False
+        '
+        'titlepic5
+        '
+        Me.titlepic5.Image = Global.Blocky_Adventure.My.Resources.Resources.spike1
+        Me.titlepic5.Location = New System.Drawing.Point(133, 367)
+        Me.titlepic5.Name = "titlepic5"
+        Me.titlepic5.Size = New System.Drawing.Size(101, 101)
+        Me.titlepic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.titlepic5.TabIndex = 8
+        Me.titlepic5.TabStop = False
+        '
+        'titlepic3
+        '
+        Me.titlepic3.Image = Global.Blocky_Adventure.My.Resources.Resources.player3
+        Me.titlepic3.Location = New System.Drawing.Point(59, 249)
+        Me.titlepic3.Name = "titlepic3"
+        Me.titlepic3.Size = New System.Drawing.Size(101, 101)
+        Me.titlepic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.titlepic3.TabIndex = 6
+        Me.titlepic3.TabStop = False
+        '
+        'titlepic2
+        '
+        Me.titlepic2.Image = Global.Blocky_Adventure.My.Resources.Resources.player1
+        Me.titlepic2.Location = New System.Drawing.Point(1025, 23)
+        Me.titlepic2.Name = "titlepic2"
+        Me.titlepic2.Size = New System.Drawing.Size(135, 135)
+        Me.titlepic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.titlepic2.TabIndex = 2
+        Me.titlepic2.TabStop = False
+        '
+        'titlepic
+        '
+        Me.titlepic.Image = Global.Blocky_Adventure.My.Resources.Resources.player1
+        Me.titlepic.Location = New System.Drawing.Point(25, 23)
+        Me.titlepic.Name = "titlepic"
+        Me.titlepic.Size = New System.Drawing.Size(135, 135)
+        Me.titlepic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.titlepic.TabIndex = 0
+        Me.titlepic.TabStop = False
+        '
+        'titlepic9
+        '
+        Me.titlepic9.Image = Global.Blocky_Adventure.My.Resources.Resources.platform
+        Me.titlepic9.Location = New System.Drawing.Point(921, 299)
+        Me.titlepic9.Name = "titlepic9"
+        Me.titlepic9.Size = New System.Drawing.Size(119, 65)
+        Me.titlepic9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.titlepic9.TabIndex = 12
+        Me.titlepic9.TabStop = False
+        '
+        'titlepic10
+        '
+        Me.titlepic10.Image = Global.Blocky_Adventure.My.Resources.Resources.platform
+        Me.titlepic10.Location = New System.Drawing.Point(823, 403)
+        Me.titlepic10.Name = "titlepic10"
+        Me.titlepic10.Size = New System.Drawing.Size(119, 65)
+        Me.titlepic10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.titlepic10.TabIndex = 13
+        Me.titlepic10.TabStop = False
+        '
+        'titlepic11
+        '
+        Me.titlepic11.Image = Global.Blocky_Adventure.My.Resources.Resources.platform
+        Me.titlepic11.Location = New System.Drawing.Point(1066, 288)
+        Me.titlepic11.Name = "titlepic11"
+        Me.titlepic11.Size = New System.Drawing.Size(119, 65)
+        Me.titlepic11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.titlepic11.TabIndex = 14
+        Me.titlepic11.TabStop = False
+        '
+        'scoretimer
+        '
+        Me.scoretimer.Enabled = True
+        Me.scoretimer.Interval = 68
+        '
+        'scorelabel
+        '
+        Me.scorelabel.Font = New System.Drawing.Font("Elephant", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.scorelabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.scorelabel.Location = New System.Drawing.Point(93, -2)
+        Me.scorelabel.Name = "scorelabel"
+        Me.scorelabel.Size = New System.Drawing.Size(169, 58)
+        Me.scorelabel.TabIndex = 15
+        Me.scorelabel.Text = "Label1"
+        '
+        'scoretext
+        '
+        Me.scoretext.Font = New System.Drawing.Font("Century", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.scoretext.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.scoretext.Location = New System.Drawing.Point(-32, -2)
+        Me.scoretext.Name = "scoretext"
+        Me.scoretext.Size = New System.Drawing.Size(169, 58)
+        Me.scoretext.TabIndex = 16
+        Me.scoretext.Text = "Score:"
+        Me.scoretext.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'platform1
+        '
+        Me.platform1.Image = Global.Blocky_Adventure.My.Resources.Resources.platform
+        Me.platform1.Location = New System.Drawing.Point(-85, 377)
+        Me.platform1.Name = "platform1"
+        Me.platform1.Size = New System.Drawing.Size(1370, 159)
+        Me.platform1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.platform1.TabIndex = 17
+        Me.platform1.TabStop = False
+        '
+        'player1
+        '
+        Me.player1.Image = Global.Blocky_Adventure.My.Resources.Resources.player1
+        Me.player1.Location = New System.Drawing.Point(-1, 356)
+        Me.player1.Name = "player1"
+        Me.player1.Size = New System.Drawing.Size(76, 76)
+        Me.player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.player1.TabIndex = 18
+        Me.player1.TabStop = False
+        '
+        'gametimer
+        '
+        Me.gametimer.Enabled = True
+        Me.gametimer.Interval = 1
+        '
+        'spike1
+        '
+        Me.spike1.Image = Global.Blocky_Adventure.My.Resources.Resources.spike1
+        Me.spike1.Location = New System.Drawing.Point(529, 391)
+        Me.spike1.Name = "spike1"
+        Me.spike1.Size = New System.Drawing.Size(42, 41)
+        Me.spike1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.spike1.TabIndex = 19
+        Me.spike1.TabStop = False
+        '
+        'platform2
+        '
+        Me.platform2.Image = Global.Blocky_Adventure.My.Resources.Resources.platform
+        Me.platform2.Location = New System.Drawing.Point(1792, 367)
+        Me.platform2.Name = "platform2"
+        Me.platform2.Size = New System.Drawing.Size(231, 169)
+        Me.platform2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.platform2.TabIndex = 20
+        Me.platform2.TabStop = False
+        '
+        'startgamelabel
+        '
+        Me.startgamelabel.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.startgamelabel.Font = New System.Drawing.Font("Forte", 35.75!)
+        Me.startgamelabel.Location = New System.Drawing.Point(432, 182)
+        Me.startgamelabel.Name = "startgamelabel"
+        Me.startgamelabel.Size = New System.Drawing.Size(253, 65)
+        Me.startgamelabel.TabIndex = 21
+        Me.startgamelabel.Text = "Start Game"
+        Me.startgamelabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'howtoplaylabel
+        '
+        Me.howtoplaylabel.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.howtoplaylabel.Font = New System.Drawing.Font("Forte", 35.75!)
+        Me.howtoplaylabel.Location = New System.Drawing.Point(391, 288)
+        Me.howtoplaylabel.Name = "howtoplaylabel"
+        Me.howtoplaylabel.Size = New System.Drawing.Size(334, 65)
+        Me.howtoplaylabel.TabIndex = 22
+        Me.howtoplaylabel.Text = "How to Play"
+        Me.howtoplaylabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'creditslabel
+        '
+        Me.creditslabel.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.creditslabel.Font = New System.Drawing.Font("Forte", 35.75!)
+        Me.creditslabel.Location = New System.Drawing.Point(432, 403)
+        Me.creditslabel.Name = "creditslabel"
+        Me.creditslabel.Size = New System.Drawing.Size(253, 65)
+        Me.creditslabel.TabIndex = 23
+        Me.creditslabel.Text = "Credits"
+        Me.creditslabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'jumptimer
+        '
+        Me.jumptimer.Enabled = True
+        Me.jumptimer.Interval = 1
+        '
+        'skintimer
+        '
+        Me.skintimer.Enabled = True
+        Me.skintimer.Interval = 175
+        '
+        'movingtimer
+        '
+        Me.movingtimer.Enabled = True
+        Me.movingtimer.Interval = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightCoral
+        Me.ClientSize = New System.Drawing.Size(1197, 524)
+        Me.Controls.Add(Me.creditslabel)
+        Me.Controls.Add(Me.howtoplaylabel)
+        Me.Controls.Add(Me.startgamelabel)
+        Me.Controls.Add(Me.player1)
+        Me.Controls.Add(Me.platform2)
+        Me.Controls.Add(Me.spike1)
+        Me.Controls.Add(Me.platform1)
+        Me.Controls.Add(Me.scorelabel)
+        Me.Controls.Add(Me.scoretext)
+        Me.Controls.Add(Me.titlepic6)
+        Me.Controls.Add(Me.titlepic11)
+        Me.Controls.Add(Me.titlepic7)
+        Me.Controls.Add(Me.titlepic10)
+        Me.Controls.Add(Me.titlepic8)
+        Me.Controls.Add(Me.titlepic9)
+        Me.Controls.Add(Me.titlepic5)
+        Me.Controls.Add(Me.titlepic3)
+        Me.Controls.Add(Me.titlepic2)
+        Me.Controls.Add(Me.titlelabel)
+        Me.Controls.Add(Me.titlepic)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.Text = "Blocky Adventure"
+        CType(Me.titlepic8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.titlepic7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.titlepic6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.titlepic5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.titlepic3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.titlepic2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.titlepic, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.titlepic9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.titlepic10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.titlepic11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.platform1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.player1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.spike1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.platform2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents titlepic As PictureBox
+    Friend WithEvents titlelabel As Label
+    Friend WithEvents titlepic2 As PictureBox
+    Friend WithEvents titlepic3 As PictureBox
+    Friend WithEvents titlepic5 As PictureBox
+    Friend WithEvents titlepic6 As PictureBox
+    Friend WithEvents titlepic7 As PictureBox
+    Friend WithEvents titlepic8 As PictureBox
+    Friend WithEvents titlepic9 As PictureBox
+    Friend WithEvents titlepic10 As PictureBox
+    Friend WithEvents titlepic11 As PictureBox
+    Friend WithEvents scoretimer As Timer
+    Friend WithEvents scorelabel As Label
+    Friend WithEvents scoretext As Label
+    Friend WithEvents platform1 As PictureBox
+    Friend WithEvents player1 As PictureBox
+    Friend WithEvents gametimer As Timer
+    Friend WithEvents spike1 As PictureBox
+    Friend WithEvents platform2 As PictureBox
+    Friend WithEvents startgamelabel As Label
+    Friend WithEvents howtoplaylabel As Label
+    Friend WithEvents creditslabel As Label
+    Friend WithEvents jumptimer As Timer
+    Friend WithEvents skintimer As Timer
+    Friend WithEvents movingtimer As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,202 @@
+﻿' Blocky Adventure
+' 5/1/23
+' Alfred
+' Period 1
+' 1213 x 569
+Imports System.Drawing.Imaging
+
+Public Class Form1
+    Dim score As Integer
+    Dim gamestart As Integer
+    Dim dy As Integer
+    Dim jump As Integer
+    Dim model As Integer
+    Dim gravity As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+
+        ' Disabling timers on form load, and setting score to 0 to essentially reset the game mechanics and score.
+
+        scoretimer.Enabled = False
+        scorelabel.Visible = False
+        scoretext.Visible = False
+        platform1.Visible = False
+        player1.Visible = False
+        score = 0
+        gamestart = 0
+        spike1.Visible = False
+        gametimer.Enabled = False
+        jump = 0
+        dy = 0
+        jumptimer.Enabled = False
+        skintimer.Enabled = False
+        movingtimer.Enabled = False
+        gravity = -5
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles scoretimer.Tick
+
+        ' Score count label counts in game counter
+
+        score = score + 1
+        scorelabel.Text = score
+        scoretext.Visible = True
+
+
+        ' Setting the max score of the level and turning off timer so no infinite glitch happens and ending level.
+
+        If score = 1000 Then
+            scoretimer.Enabled = False
+            MsgBox("You beat the level! Congrats")
+            End
+        End If
+
+
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+
+        ' To exit the game/level then press Q/Escape Key.
+
+        If e.KeyCode = Keys.Q Or e.KeyCode = Keys.Escape Then End
+
+        ' Once a jump key is pressed, the jumptimer goes off
+
+        If e.KeyCode = Keys.Up Then
+            jump = 1
+            jumptimer.Enabled = True
+        End If
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles gametimer.Tick
+
+        ' Animates the background to be moving, rather than the player.
+
+        platform1.Left = platform1.Left - 2.5
+        platform2.Left = platform2.Left - 2.5
+        spike1.Left = spike1.Left - 2.5
+
+
+    End Sub
+
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        If jump = 1 Then
+            jump = 0
+        End If
+    End Sub
+
+    ' Title Screen Phase of Game
+    Private Sub howtoplaylabel_Click(sender As Object, e As EventArgs) Handles howtoplaylabel.Click
+        ' Msgboxing because it makes it so they have to click out and does not cause any problems.
+        MsgBox("Up Arrow Key to jump, avoid spikes and jump on platforms and perfectly time your way around and over these obstacles to reach the end of the level! The end of the each level is when you get 1000 for your score!")
+    End Sub
+
+    Private Sub creditslabel_Click(sender As Object, e As EventArgs) Handles creditslabel.Click
+        MsgBox("Credits to me, Carti, and SZA for making all the code of the project, credits to all of the supporters, such as New York Times, Bonton, and more for the motivation to push forward, and thanks to the critics who complimented our game, Hails, Luke Sherry, and Ethan.")
+    End Sub
+
+    Private Sub startgamelabel_Click(sender As Object, e As EventArgs) Handles startgamelabel.Click
+
+        ' Hides all the interface from the loading screen to basically start the game and create an empty canvas
+        titlepic.Visible = False
+        titlepic2.Visible = False
+        titlepic3.Visible = False
+        titlepic5.Visible = False
+        titlepic6.Visible = False
+        titlepic7.Visible = False
+        titlepic8.Visible = False
+        titlepic9.Visible = False
+        titlepic10.Visible = False
+        titlepic11.Visible = False
+        titlelabel.Visible = False
+
+        ' Made these labels so that I can use keyup and kewdown commands.
+        startgamelabel.Visible = False
+        howtoplaylabel.Visible = False
+        creditslabel.Visible = False
+
+        ' Starting in game timer and revealing in game stuff.
+
+        scorelabel.Visible = True
+        scoretimer.Enabled = True
+        platform1.Visible = True
+        player1.Visible = True
+        spike1.Visible = True
+        gametimer.Enabled = True
+        movingtimer.Enabled = True
+        gamestart = 1
+    End Sub
+
+    ' Next Section is just going to be coloring Labels when mouse enters/leaves to make it look nicer.
+
+    Private Sub startgamelabel_MouseEnter(sender As Object, e As EventArgs) Handles startgamelabel.MouseEnter
+        startgamelabel.BackColor = Color.Pink
+    End Sub
+
+    Private Sub startgamelabel_MouseLeave(sender As Object, e As EventArgs) Handles startgamelabel.MouseLeave
+        startgamelabel.BackColor = Color.White
+    End Sub
+
+    Private Sub howtoplaylabel_MouseEnter(sender As Object, e As EventArgs) Handles howtoplaylabel.MouseEnter
+        howtoplaylabel.BackColor = Color.Pink
+    End Sub
+
+    Private Sub howtoplaylabel_MouseLeave(sender As Object, e As EventArgs) Handles howtoplaylabel.MouseLeave
+        howtoplaylabel.BackColor = Color.White
+    End Sub
+
+    Private Sub creditslabel_MouseEnter(sender As Object, e As EventArgs) Handles creditslabel.MouseEnter
+        creditslabel.BackColor = Color.Pink
+    End Sub
+
+    Private Sub creditslabel_MouseLeave(sender As Object, e As EventArgs) Handles creditslabel.MouseLeave
+        creditslabel.BackColor = Color.White
+    End Sub
+
+    Private Sub jumptimer_Tick(sender As Object, e As EventArgs) Handles jumptimer.Tick
+
+        '  As long as they are jumping gravity is added
+
+        If jump = 1 Then
+            player1.Top = player1.Top - gravity
+            gravity = -2
+            player1.Top = player1.Top - 6
+        End If
+
+        ' As long as they are on the ground then jumping is disabled.
+
+        If player1.Top - player1.Height <= platform1.Top Then
+            jump = 0
+        End If
+
+        ' As long as they are jumping the skins will change direction
+
+        If jumptimer.Enabled = True Then
+            skintimer.Enabled = True
+        Else
+            skintimer.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub skintimer_Tick(sender As Object, e As EventArgs) Handles skintimer.Tick
+
+        ' while jumping the skin will change direction randomly so it's not bland
+
+        model = Int(Rnd() * 4)
+
+        If model = 1 Then
+            player1.Image = My.Resources.player1
+        ElseIf model = 2 Then
+            player1.Image = My.Resources.player2
+        ElseIf model = 3 Then
+            player1.Image = My.Resources.player3
+        ElseIf model = 4 Then
+            player1.Image = My.Resources.player4
+        End If
+
+    End Sub
+End Class
